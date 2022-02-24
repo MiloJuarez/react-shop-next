aa92cc81d268b750e5165345ab9e0cef8eae3e43
Milo Juarez <ermilo98go@gmail.com>
2022-02-24 12:53:34 -0600

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    env: {
+        customKey: 'customValue',
+    },
+    basePath: '/dist',
+    compress: true,
+    async redirects() {
+        return [
+            {
+                source: '/hola',
+                destination: '/hello',
+                permanent: true,
+            },
+        ];
+    },
+};
