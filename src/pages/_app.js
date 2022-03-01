7a04caced9a2e815b78ba87ff5fe92a2618a1db0
Milo Juarez <ermilo98go@gmail.com>
2022-03-01 12:32:04 -0600

    Fixed scripts warning in _document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
-import '@styles/globals.css';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 import Header from '@components/Header';
+import Script from 'next/script';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
     const initialState = useInitialState();
     return (
         <AppContext.Provider value={initialState}>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SHEVQMVFQ7" />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-SHEVQMVFQ7');
+                `}
+            </Script>
             <Header />
             <Component {...pageProps} />
         </AppContext.Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,15 +5,7 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SHEVQMVFQ7"></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-SHEVQMVFQ7');`,
-                        }}
-                    />
+                    <meta charSet="utf-8" />
                 </Head>
                 <body>
                     {/* Donde se contendran el contenido de la aplicacion */}
