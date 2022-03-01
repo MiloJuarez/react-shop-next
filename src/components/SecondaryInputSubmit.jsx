579dc3becdce16296590ddc23d4cf0442eada49a
Milo Juarez <ermilo98go@gmail.com>
2022-03-01 12:51:17 -0600

    Fixed lint warning: replace

--- src/components/SecondaryInputSubmit.jsx
+++ src/components/SecondaryInputSubmit.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import "@styles/SecondaryInputSubmit.scss";
+import React from 'react';
+import styles from '@styles/SecondaryInputSubmit.module.scss';
 
 const SecondaryInputSubmit = ({ value }) => {
-    return (
-        <input type="submit" value={value} className="SecondaryInputSubmit" />
-    );
+    return <input type="submit" value={value} className={styles.SecondaryInputSubmit} />;
 };
 
 export default SecondaryInputSubmit;
