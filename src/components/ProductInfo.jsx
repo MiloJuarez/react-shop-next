aa9bc0a030a043057782fcf111bbadaa397e5ae8
Milo Juarez <ermilo98go@gmail.com>
2022-03-01 12:46:51 -0600

    Fixed lint warnings: use Image, replaces

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,27 +1,24 @@
-import React from "react";
-import "@styles/ProductInfo.scss";
-import addToCard from "@icons/bt_add_to_cart.svg";
+import React from 'react';
+import addToCard from '@icons/bt_add_to_cart.svg';
+import styles from '@styles/ProductInfo.module.scss';
+import Image from 'next/image';
 
 const ProductInfo = ({ product }) => {
     return (
         <>
-            <img
-                src={product.image[0]}
-                alt={product.title}
-                className="ProductInfo__image"
-            />
-            <div className="ProductInfo">
-                <p className="ProductInfo__text ProductInfo__text--small ProductInfo__text--bold ProductInfo__text--mb4">
-                    ${product.price}
-                </p>
-                <p className="ProductInfo__text ProductInfo__text--medium ProductInfo__text--bold ProductInfo__text--light ProductInfo__text--mb36">
+            <Image src={product.image[0]} alt={product.title} className={styles.ProductInfo__image} />
+            <div className={styles.ProductInfo}>
+                <p className={(styles['ProductInfo__text'], styles['ProductInfo__text--small'], styles['ProductInfo__text--bold'], styles['ProductInfo__text--mb4'])}>${product.price}</p>
+                <p
+                    className={
+                        (styles['ProductInfo__text'], styles['ProductInfo__text--medium'], styles['ProductInfo__text--bold'], styles['ProductInfo__text--light'], styles['ProductInfo__text--mb36'])
+                    }
+                >
                     {product.title}
                 </p>
-                <p className="ProductInfo__text ProductInfo__text--small ProductInfo__text--mb36">
-                    {product.description}
-                </p>
-                <button className="ProductInfo__button ProductInfo__button--flex">
-                    <img src={addToCard} alt="add to cart" />
+                <p className={(styles['ProductInfo__text'], styles['ProductInfo__text--small'], styles['ProductInfo__text--mb36'])}>{product.description}</p>
+                <button className={(styles['ProductInfo__button'], styles['ProductInfo__button--flex'])}>
+                    <Image src={addToCard} alt="add to cart" />
                     Add to cart
                 </button>
             </div>
